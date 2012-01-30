4247400a55bc08d83bd8ac0f72d22196086c5754
Josh Davis <joshuadavis42@gmail.com>
2012-01-30 17:42:31 -0600

    Added Supertab to bundles

--- updateBundles.sh
+++ updateBundles.sh
@@ -9,11 +9,12 @@ builtin cd bundle
 
 # Vim plugins
 git clone https://github.com/scrooloose/nerdtree.git
-git clone git://github.com/garbas/vim-snipmate.git
+git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/snipmate-snippets.git
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/tomtom/tlib_vim.git
-git clone git://github.com/msanders/cocoa.vim.git
+git clone https://github.com/msanders/cocoa.vim.git
+git clone https://github.com/ervandew/supertab.git
 
 # Update all of the bundles just in case.
 for path in `ls ~/.vim/bundle`
