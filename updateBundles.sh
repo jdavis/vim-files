3cc129843cecc7e1610103c574cd09ec1bb84714
Josh Davis <joshuadavis42@gmail.com>
2012-02-19 13:56:07 -0600

    Added a few bundles and removed one

--- updateBundles.sh
+++ updateBundles.sh
@@ -20,7 +20,8 @@ git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/tpope/vim-surround.git
 git clone https://github.com/edsono/vim-matchit.git
 git clone https://github.com/Raimondi/delimitMate.git
-git clone https://github.com/docunext/closetag.vim.git
+#git clone https://github.com/docunext/closetag.vim.git
+git clone https://github.com/sukima/xmledit.git
 
 # Update all of the bundles just in case.
 for path in `ls ~/.vim/bundle`
