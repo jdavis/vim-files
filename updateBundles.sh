1c5454de429a46a60c635efc9ddf7e955df3966e
Josh Davis <joshuadavis42@gmail.com>
2012-05-25 03:06:32 -0500

    Cleaned up my plugins in my updateBundles.sh script

--- updateBundles.sh
+++ updateBundles.sh
@@ -8,27 +8,50 @@ fi
 builtin cd bundle
 
 # Vim plugins
+# For file browsing
 git clone https://github.com/scrooloose/nerdtree.git
+git clone https://github.com/jistr/vim-nerdtree-tabs.git
+
+# For the best snippet functionality
 git clone https://github.com/garbas/vim-snipmate.git
 git clone https://github.com/honza/snipmate-snippets.git
+
+# Required for snipMate
 git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
 git clone https://github.com/tomtom/tlib_vim.git
-git clone https://github.com/msanders/cocoa.vim.git
+
+# For tab completion
 git clone https://github.com/ervandew/supertab.git
-#git clone https://github.com/scrooloose/snipmate-snippets.git scrooloose-snippets
+
+# Various commenting capabilities
 git clone https://github.com/scrooloose/nerdcommenter.git
-git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/edsono/vim-matchit.git
+
+# I don't use this to it's full capabilities...
+#git clone https://github.com/tpope/vim-surround.git
+
+# Matches stuff... lol
+#git clone https://github.com/edsono/vim-matchit.git
+
+# Autocloses characters
+git clone https://github.com/Townk/vim-autoclose.git
+
+# For checking the syntax of any file
+git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/wincent/Command-T.git
+
+# For JavaScript checking
+git clone https://github.com/hallettj/jslint.vim.git
+
+# Vim and Git, sayyyy whatttt
+git clone https://github.com/tpope/vim-fugitive.git
+
+# Old and stale plugins
 #git clone https://github.com/Raimondi/delimitMate.git
 #git clone https://github.com/docunext/closetag.vim.git
 #git clone https://github.com/sukima/xmledit.git
-git clone https://github.com/Townk/vim-autoclose.git
-git clone https://github.com/jistr/vim-nerdtree-tabs.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/wincent/Command-T.git
-git clone https://github.com/majutsushi/tagbar.git
-git clone https://github.com/hallettj/jslint.vim.git
-git clone https://github.com/tpope/vim-fugitive.git
+#git clone https://github.com/msanders/cocoa.vim.git
+#git clone https://github.com/scrooloose/snipmate-snippets.git scrooloose-snippets
+#git clone https://github.com/majutsushi/tagbar.git
 
 
 # Update all of the bundles just in case.
